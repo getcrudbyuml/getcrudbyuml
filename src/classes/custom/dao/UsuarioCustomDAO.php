50a57284e0f18ec9a08f1054b1054674f23150c9
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-09-16 15:39:49 -0300

    Evitando SQL INjection.

--- src/classes/custom/dao/UsuarioCustomDAO.php
+++ src/classes/custom/dao/UsuarioCustomDAO.php
@@ -9,19 +9,32 @@
 
 class  UsuarioCustomDAO extends UsuarioDAO {
     
+
+    
     public function autentica(Usuario $usuario){
         $login = $usuario->getLogin();
         $senha = $usuario->getSenha() ;
-        $sql = "SELECT * FROM usuario WHERE (login  ='$login' AND senha = '$senha') OR (email ='$login' AND senha = '$senha') LIMIT 1";
+        $sql = "SELECT * FROM usuario WHERE login  = :login AND senha = :senha LIMIT 1";
         
-        foreach ( $this->getConexao ()->query ( $sql ) as $linha ) {
-            $usuario->setLogin ( $linha ['login'] );
-            $usuario->setEmail( $linha ['email'] );
-            $usuario->setId( $linha ['id'] );
-            $usuario->setNivel( $linha ['nivel'] );
-            return true;
+        try {
+            
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->bindParam(":login", $login, PDO::PARAM_STR);
+            $stmt->bindParam(":senha", $senha, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ( $result as $linha ) {
+                $usuario->setLogin ( $linha ['login'] );
+                $usuario->setId( $linha ['id'] );
+                $usuario->setNivel( $linha ['nivel'] );
+                return true;
+            }
+            return false;
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            return false;
         }
-        return false;
+        
         
     }
     
