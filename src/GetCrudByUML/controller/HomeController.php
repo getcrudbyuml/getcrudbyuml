e5bb822f071e1932ec22814947a4b3b14d49e2ff
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-10-07 17:34:57 -0300

    Internacionalização da doação.

--- src/GetCrudByUML/controller/HomeController.php
+++ src/GetCrudByUML/controller/HomeController.php
@@ -127,17 +127,44 @@ class HomeController{
         }
         //
                      
-                     echo '</p>
-                    
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+         echo '</p>';
+                     
+                
+         if(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) == 'pt'){
+             echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick" />
-<input type="hidden" name="hosted_button_id" value="4M3VMZUYC8YTQ" />
+<input type="hidden" name="hosted_button_id" value="YZ2P3PA58NHJ2" />
+<input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1" />
+</form>
+';//PT Button
+         }else{
+             //English Button
+             echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="3URFYYWL8QUG4" />
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_BR/i/scr/pixel.gif" width="1" height="1" />
 </form>
+
+';//English Button
+         }
+         
+         
+                     
+         echo '
 <br>
 <br>
-<img src="images/qr_code.png" height="200" alt="QR Code">
+<img src="';
+
+         
+         if(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) == 'pt'){
+             echo 'images/qr_code_pt.png';
+         }else{
+             echo 'images/qr_code.png';
+         }
+        
+echo '" height="200" alt="QR Code">
 <br>
 <br>
 
