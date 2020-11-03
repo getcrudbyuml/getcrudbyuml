0219d1bbbb68d2bb246bc5f3fb3a857126029984
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-11-03 08:54:10 -0300

    Umas correções.

--- src/GetCrudByUML/controller/HomeController.php
+++ src/GetCrudByUML/controller/HomeController.php
@@ -96,7 +96,7 @@ class HomeController{
                 
                 
                 echo '</p>
-                <img  class="img-fluid" src="';
+                <img src="';
     if(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) == 'pt'){
         echo './images/classes.png';
     }else{
@@ -129,7 +129,7 @@ class HomeController{
                 
                 echo '</p>
 
-                <img  class="img-fluid" src="';
+                <img  src="';
                 if(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) == 'pt'){
                     echo './images/sql2.png';
                 }else{
@@ -168,8 +168,8 @@ class HomeController{
              echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick" />
 <input type="hidden" name="hosted_button_id" value="YZ2P3PA58NHJ2" />
-<input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-<img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1" />
+<input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt=""  src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1" />
 </form>
 ';//PT Button
          }else{
