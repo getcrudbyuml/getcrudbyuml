7d87b7dd8f2b9007c5bc4e499465d47ebee04838
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-11-11 15:27:34 -0300

    Melhoria no blog.

--- src/blogGetCrudByUML/custom/dao/PostsCustomDAO.php
+++ src/blogGetCrudByUML/custom/dao/PostsCustomDAO.php
@@ -9,6 +9,8 @@
 namespace blogGetCrudByUML\custom\dao;
 use blogGetCrudByUML\dao\PostsDAO;
 use blogGetCrudByUML\model\Posts;
+use PDO;
+use PDOException;
 
 
 class  PostsCustomDAO extends PostsDAO {
@@ -26,85 +28,134 @@ class  PostsCustomDAO extends PostsDAO {
         $lista = array ();
         $sql = "SELECT * FROM ".PREFIXO_WP."posts
         WHERE post_status = 'publish'
-        AND post_type = '$tipo'
-         ORDER BY ID DESC LIMIT $limit";
-        $result = $this->getConnection()->query ( $sql );
+        AND post_type = :tipo
+         ORDER BY ID DESC LIMIT :limit";
         
-        foreach ( $result as $linha ) {
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindParam(":tipo", $tipo, PDO::PARAM_STR);
+            $stmt->bindParam(":limit", $limit, PDO::PARAM_INT);
+            $stmt->execute();
             
-            $post = new Posts();
-            
-            $post->setID( $linha ['ID'] );
-            $post->setContent(utf8_encode (  $linha ['post_content'] ));
-            $post->setTitle(utf8_encode (   $linha ['post_title']) );
-            $post->setDate( $linha ['post_date'] );
-            $lista [] = $post;
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ( $result as $linha)
+            {
+                $post = new Posts();
+                
+                $post->setID( $linha ['ID'] );
+                $post->setContent(utf8_encode (  $linha ['post_content'] ));
+                $post->setTitle(utf8_encode (   $linha ['post_title']) );
+                $post->setDate( $linha ['post_date'] );
+                $lista [] = $post;
+            }
+        } catch(PDOException $e) {
+            echo $e->getMessage();
         }
+        
         return $lista;
     }
     
     public function pesquisaPorID(Posts $post) {
+
         $lista = array();
-        $ID = $post->getID();
-        $sql = "SELECT * FROM ".PREFIXO_WP."posts
-                 WHERE ID = $ID";
-        $result = $this->getConnection ()->query ( $sql );
+        $id = $post->getId();
         
-        foreach ( $result as $linha ) {
-            $post->setID( $linha ['ID'] );
-            $post->setContent(utf8_encode (  $linha ['post_content'] ));
-            $post->setTitle( utf8_encode ( $linha ['post_title'] ));
-            $post->setDate( $linha ['post_date'] );
-            $lista [] = $post;
+        $sql = "SELECT * FROM ".PREFIXO_WP."posts
+                 WHERE ID = :id";
+        try {
+            
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ( $result as $linha ){
+                $post->setID( $linha ['ID'] );
+                $post->setContent(utf8_encode (  $linha ['post_content'] ));
+                $post->setTitle( utf8_encode ( $linha ['post_title'] ));
+                $post->setDate( $linha ['post_date'] );
+                $lista [] = $post;
+                
+                
+            }
+            
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            
         }
         return $lista;
     }
     
     public function pesquisaPorContent(Posts $post) {
         $lista = array();
-        $content = strtolower($post->getContent());
+        $content = '%'.$post->getContent().'%';
+        
         $sql = "SELECT * FROM ".PREFIXO_WP."posts
         WHERE
         post_status = 'publish'
         AND post_type = 'post'
-        AND LOWER(post_content) like '%$content%'
+        AND LOWER(post_content) like :content
         ORDER BY ID DESC LIMIT 1000";
         
-        $result = $this->getConnection ()->query ( $sql );
-        
-        foreach ( $result as $linha ) {
-            $post2 = new Posts();
-            $post2->setID( $linha ['ID'] );
-            $post2->setContent(utf8_encode (  $linha ['post_content'] ));
-            $post2->setTitle( utf8_encode ( $linha ['post_title'] ));
-            $post2->setDate( $linha ['post_date'] );
-            $lista [] = $post2;
+        try {
+            
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindParam(":content", $content, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ( $result as $linha ){
+                $post2 = new Posts();
+                $post2->setID( $linha ['ID'] );
+                $post2->setContent(utf8_encode (  $linha ['post_content'] ));
+                $post2->setTitle( utf8_encode ( $linha ['post_title'] ));
+                $post2->setDate( $linha ['post_date'] );
+                $lista [] = $post2;
+                
+            }
+            
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            
         }
         return $lista;
+        
+        
+        
     }
     
-    public function pesquisaPorTitle(Posts $post) {
+    public function pesquisaPorTitle(Posts $post) {        
         $lista = array();
-        $title = strtolower($post->getTitle());
+        $title = '%'.$post->getTitle().'%';
+        
         $sql = "SELECT * FROM ".PREFIXO_WP."posts
                  WHERE
                 post_status = 'publish'
                 AND post_type = 'post'
-                AND LOWER(post_title) like '%$title%'
+                AND LOWER(post_title) like :title
                  ORDER BY ID DESC LIMIT 1000";
         
-        
-        $result = $this->getConnection ()->query ( $sql );
-        
-        foreach ( $result as $linha ) {
-            $post2 = new Posts();
-            $post2->setID( $linha ['ID'] );
-            $post2->setContent(utf8_encode (  $linha ['post_content'] ));
-            $post2->setTitle( utf8_encode ( $linha ['post_title'] ));
-            $post2->setDate( $linha ['post_date'] );
-            $lista [] = $post2;
+        try {
+            
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindParam(":title", $title, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ( $result as $linha ){
+                $post2 = new Posts();
+                $post2->setID( $linha ['ID'] );
+                $post2->setContent(utf8_encode (  $linha ['post_content'] ));
+                $post2->setTitle( utf8_encode ( $linha ['post_title'] ));
+                $post2->setDate( $linha ['post_date'] );
+                $lista [] = $post2;
+                
+                
+            }
+            
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            
         }
         return $lista;
+        
     }
     
     
