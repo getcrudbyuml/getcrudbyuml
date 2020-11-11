23de3bb529d761daea859f5ed7082a588d3eae3c
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-11-11 15:35:20 -0300

    Evitando um erro.

--- src/blogGetCrudByUML/model/Posts.php
+++ src/blogGetCrudByUML/model/Posts.php
@@ -28,6 +28,10 @@ class Posts {
 	    $this->content = $content;
 	    $html = new \simple_html_dom($content);
 	    $i = 0;
+	    if($html == null){
+	        $this->content = '';
+	        return;
+	    }
 	    foreach($html->find('img') as $img)
 	    {
 	        $search = 'http://dti.unilab.edu.br/wp-content';
